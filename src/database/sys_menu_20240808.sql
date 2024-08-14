057685b32af1808f11428306e0b36decee8c09d3
i5NWkv9NH1 <sloananna326@gmail.com>
2024-08-15 01:32:40 +0900

    fix: remove unused anything

--- src/database/sys_menu_20240808.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "sys_menu" ("id", "label", "router", "icon", "path", "parent_id", "createdAt", "updatedAt")
-VALUES 
-('71ebac63-97e3-468c-ae3b-7b86c437028f', 'System', 'system', 'mdi-view-dashboard', '71ebac63-97e3-468c-ae3b-7b86c437028f', NULL, '2024-08-07 15:44:33.294756', '2024-08-07 15:44:33.294756'),
-('43743a12-6165-4fb9-9118-57169f685145', 'Roles', 'roles', 'mdi-account-cog', '71ebac63-97e3-468c-ae3b-7b86c437028f.43743a12-6165-4fb9-9118-57169f685145', '71ebac63-97e3-468c-ae3b-7b86c437028f', '2024-08-07 15:44:33.294756', '2024-08-07 15:44:33.294756'),
-('7e878b18-ff39-4622-8e15-e8b99e0235f8', 'Organizations', 'organizations', 'mdi-domain', '71ebac63-97e3-468c-ae3b-7b86c437028f.7e878b18-ff39-4622-8e15-e8b99e0235f8', '71ebac63-97e3-468c-ae3b-7b86c437028f', '2024-08-07 15:44:33.294756', '2024-08-07 15:44:33.294756'),
-('74d74b3c-97d9-4446-b3b2-054cd496eaba', 'Accounts', 'accounts', 'mdi-account-group', '71ebac63-97e3-468c-ae3b-7b86c437028f.74d74b3c-97d9-4446-b3b2-054cd496eaba', '71ebac63-97e3-468c-ae3b-7b86c437028f', '2024-08-07 15:44:33.294756', '2024-08-07 15:44:33.294756');
